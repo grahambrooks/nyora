21aa6d6970e6e9879b7baf25a5a7eab77d725c98
Graham Brooks <graham@grahambrooks.com>
2012-03-12 09:00:36 -0400

    Initial ruby version for testing

---
+++ bin/nyora.rb
@@ -0,0 +1,99 @@
+
+module Nyora
+
+  class LineTagger
+    def initialize(start_line, end_line, set)
+      @tags            = []
+      @high_water_mark = 1
+      add_block start_line, end_line, set
+    end
+    
+    def add_block(start_line, end_line, set)
+      @tags << [@high_water_mark, start_line, end_line, set]
+      @high_water_mark += 1
+    end
+    
+    def tag_line(line_number, output, current_filename)
+      @tags.each do |tag|
+        if line_number == tag[1]
+          output.puts "//" * 50
+          output.puts "// TODO: START Duplication block #{tag[0]}"
+          tag[3].print(output, current_filename)
+        end
+        output.puts "// TODO: END Duplication Block #{tag[0]}" if line_number == tag[2]
+      end
+    end
+  end
+  
+  class TaggedSet
+    def initialize
+    @set = []
+    end
+    
+    def add(filename, start_line)
+      @set << [filename, start_line]
+    end
+    
+    def print(stream, current_filename)
+    @set.each do |filename, start_line|
+        stream.puts "// #{File.basename(filename)} around line #{start_line}" unless filename == current_filename
+      end
+    end
+  end
+  
+  class DuplicateTagger
+    def initialize
+    end
+    
+    def self.parse_emacs_format_log_file(input_file)
+      files_with_duplicates = {}
+      current_tagset        = TaggedSet.new
+      
+      File.open input_file do |f|
+        f.each_line do |line|
+          if line =~ / ([^:]+):(\d+):1:(\d+):1:/
+            current_tagset.add $1, $2
+            if files_with_duplicates[$1].nil?
+              files_with_duplicates[$1] = LineTagger.new($2.to_i, $3.to_i, current_tagset)
+            else
+            files_with_duplicates[$1].add_block $2.to_i, $3.to_i, current_tagset
+            end
+          else
+            current_tagset = TaggedSet.new
+          end
+        end
+      end
+      files_with_duplicates
+    end
+    
+    
+    def self.tag_file(filename, tags)
+      puts "Tagging #{filename}"
+      
+      File.open(filename) do |input|
+        File.open(filename + '.tagged', "w") do |output|
+          line_number = 1
+          
+          input.each_line do |line|
+            tags.tag_line(line_number, output, filename)
+            output.print line
+            line_number += 1
+          end
+          
+        end
+      end
+    end
+    
+    def self.tag(input_file)
+      if input_file.nil?
+        puts "Usage duplicate-tagger.rb emacs-formatted-duplicate-report-file"
+      else
+        files_with_duplicates = parse_emacs_format_log_file(input_file)
+        
+        files_with_duplicates.each do |filename, tags|
+          tag_file filename, tags
+        end
+      end
+    end
+  end
+end
